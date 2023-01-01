64d5e231b5e19ea6c9c84ae0d31f1c59bcc105f2
unknown <aba884682@gmail.com>
2023-01-01 07:53:20 +0200

    initial deploy

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,7 +11,7 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port:Number(process.env.POSTGRES_PORT),
+  port:Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: 'postgres',
   aws_region: process.env.AWS_REGION,
